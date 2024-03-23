17826b280130693cdb4bf7c5cfdcacbc2a584fc3
Sarah Hägele <sarah.haegele01@gmail.com>
2024-03-23 19:23:29 +0100

    feat(codium): added extensions

--- home/vscodium.nix
+++ home/vscodium.nix
@@ -6,6 +6,19 @@
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "beardedtheme";
+        publisher = "beardedbear";
+        version = "9.1.4";
+        sha256 = "sha256-RL6Yko0ustm/4Ery/JbNOaZUiQgvSbvQvd4SmQf8oWM=";
+      }
+      {
+        name = "vscode-tailwindcss";
+        publisher = "bradlc";
+        version = "0.10.2";
+        sha256 = "sha256-zh6l1smavdGp7/5yNFVOhGnWZWU/Reb5rJz1D1EY1DU=";
+      }
     ];
     userSettings = {
       "window.titleBarStyle" = "custom";
